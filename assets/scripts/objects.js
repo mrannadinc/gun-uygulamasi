183ea48a0ae2576def0da0149d456384f45ae9ca
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-26 19:32:16 +0300

    commit

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,28 +1,35 @@
 const addParticipantBtn = document.getElementById("add-btn");
 const choiseBtn = document.getElementById("random-btn");
 const clearBtn = document.getElementById("clear-btn");
-const movieList = document.getElementById('random-list');
+const participantList = document.getElementById('normal-list');
+const randomParticipantList = document.getElementById('random-list');
 
-const movies = [];
+const participant = [];
+const copyParticipant = [];
+let nextRandom = -1;
 
 const clearParticipants = () => {
-  if (movies.length === 0) {
-    movieList.classList.remove('visible');
+  if (participant.length === 0) {
+    participantList.classList.remove('visible');
+    randomParticipantList.classList.remove('visible');
   } else {
-    movieList.classList.add('visible');
+    participantList.classList.add('visible');
+    randomParticipantList.classList.add('visible');
   }
-  movieList.innerHTML = '';
-  movies.splice(0, movies.length);
+  participantList.innerHTML = '';
+  randomParticipantList.innerHTML = '';
+  participant.splice(0, participant.length);
+  copyParticipant.splice(0, copyParticipant.length);
 };
 
-const renderRandomParticipant = () => { //it is a default value while noting coming to the function
-  if (movies.length === 0) {
-    movieList.classList.remove('visible');
+const renderParticipant = () => { //it is a default value while noting coming to the function
+  if (participant.length === 0) {
+    participantList.classList.remove('visible');
   } else {
-    movieList.classList.add('visible');
+    participantList.classList.add('visible');
   }
-  movieList.innerHTML = '';
-  movies.forEach((movie) => {
+  participantList.innerHTML = '';
+  participant.forEach((movie) => {
     const movieEl = document.createElement('li');
     const { info } = movie;
     // const { title: movieTitle } = info;
@@ -35,9 +42,34 @@ const renderRandomParticipant = () => { //it is a default value while noting com
       }
     }
     movieEl.textContent = text;
-    movieList.append(movieEl); //very important section
+    participantList.append(movieEl); //very important section
   });
+  //console.log(participant);
+};
 
+const renderRandomParticipant = () => { //it is a default value while noting coming to the function
+  if (copyParticipant.length === 0) {
+    randomParticipantList.classList.remove('visible');
+  } else {
+    randomParticipantList.classList.add('visible');
+  }
+  randomParticipantList.innerHTML = '';
+  copyParticipant.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    const { info } = movie;
+    // const { title: movieTitle } = info;
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.call(movie);
+    for (const key in info) {
+      if (key !== 'title' && key !== '_title') {
+        text = text + `${key}`;
+      }
+    }
+    movieEl.textContent = text;
+    randomParticipantList.append(movieEl); //very important section
+  });
+  //console.log(participant);
 };
 
 const addParticipantHandler = () => {
@@ -65,13 +97,32 @@ const addParticipantHandler = () => {
   newMovie.info.title = title; //getter - setter 
   // console.log(newMovie.info.title);
 
-  movies.push(newMovie);
-  renderRandomParticipant();
+  participant.push(newMovie);
+  renderParticipant();
 };
 
 const randomChoiseHandler = () => {
-  const filterTerm = document.getElementById('filter-title').value;
-  renderRandomParticipant();
+  let index = Math.floor(Math.random() * participant.length); 
+  
+  if (participant.length !== 0){
+    let arrayValue = participant[index];
+    console.log(arrayValue);
+    let index2 = participant.indexOf(arrayValue);
+    console.log(index, arrayValue);
+    participant.splice(index2, 1);
+    copyParticipant.push(arrayValue);
+    renderRandomParticipant();
+  } else {
+    alert ("Kura Sonuçlandı");
+  }
+  /*if (random !== nextRandom) {
+    randomValue.push(random);
+    const randomParticipant = participant[random];
+    console.log(random, randomParticipant);
+    participant.splice(randomParticipant,1);
+    nextRandom = random;
+    randomValue.splice(random,1);
+  }*/
 };
 
 const clearParticipantHandler = () => {
